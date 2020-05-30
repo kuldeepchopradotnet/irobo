7f3d309f63b25eba82f7ab0e8ec34d87228fccd0
Kuldeep chopra <Hello@SOB-KULDEEP-15>
2020-05-30 14:41:25 +0530

    addedTokenInput

--- irobo.js
+++ irobo.js
@@ -1,16 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    /**
+     * Page title
+     */
     var pageTitle = $(".post-title.entry-title");
     var postBody = '';
     pageTitle = pageTitle.length > 0 ? pageTitle.get(0).textContent.trim() : '';
+
+    /**
+     * Pages/Post list 
+     * start
+     */
     var pagetbl = [{
-            pageName: 'what is my ip address',
-            pageNumner: 1
-        },
-        {
-            pageName: 'covid-19 update',
-            pageNumner: 2
-        }
-    ];
+        pageName: 'what is my ip address',
+        pageNumner: 1
+    },
+    {
+        pageName: 'covid-19 update',
+        pageNumner: 2
+    }];
 
     var pageNumner = getPageNumber(pageTitle);
     postBody = pageNumner > 0 ? $(".post-body.entry-content") : '';
@@ -26,19 +33,29 @@ $(document).ready(function() {
 
     }
 
-    /*All page function as componet listed here*/
-    //componet_1
+    /**
+     * All page function as components listed here
+     */
+
+    /**
+     * find my ip component
+     */
     function IpComponent() {
         var html = '<h3>Your ip address is : {ip}<h3>';
-        $.getJSON('https://api.ipify.org?format=json', function(data) {
+        $.getJSON('https://api.ipify.org?format=json', function (data) {
             html = html.replace('{ip}', (data.ip ? data.ip : ''));
             postBody.html(html);
         });
     }
-    //componet_2
-    function CovidComponent() {
 
-        $.getJSON('https://api.covid19api.com/summary', function(data) {
+    /**
+     * covidUpdate component
+     */
+    function CovidComponent() {
+        /**
+         * Get CovidData
+         */
+        $.getJSON('https://api.covid19api.com/summary', function (data) {
             var covid = data;
             var countryCovid = covid["Countries"];
             var globalCovid = covid["Global"];
@@ -47,11 +64,13 @@ $(document).ready(function() {
             var availableCountry = getAvailableCounty(countryCovid);
             CountryDomE.append(availableCountry);
             getCountryView('');
-            CountryDomE.change(function() {
+            CountryDomE.change(function () {
                 getCountryView($(this).val());
             });
-
-
+            /**
+             * Get View of CovidByCountry
+             * @param {countryName} val 
+             */
             function getCountryView(val) {
                 if (!val) {
                     if (globalCovid) {
@@ -80,36 +99,51 @@ $(document).ready(function() {
                     }
                 }
             }
-
-
+            /**
+             * GetAvailableCounty
+             * @param {CountryFieldCovid_Json} countryCovid 
+             */
             function getAvailableCounty(countryCovid) {
                 if (countryCovid && countryCovid.length > 0) {
-                    var countryDDHtml = "";
-                    countryCovid.forEach(function(country) {
-                        countryDDHtml += '<option value="' + country.Country + '">' + country.Country + '</option>';
-                    });
-                    return countryDDHtml;
+                    // var countryDDHtml = "";
+                    // countryCovid.forEach(function (country) {
+                    //     countryDDHtml += '<option value="' + country.Country + '">' + country.Country + '</option>';
+                    // });
+                    // return countryDDHtml;
+
+                    var countryList = [];
+                    // countryCovid.forEach(function (country) {
+                    //     countryList.push
+                    // });
+                    PlugTokenInput("CountryDD", countryCovid);
                 } else {
                     return null;
                 }
             }
-
+            /**
+             * Get CovidData by Country name
+             * @param {Country name} countryName 
+             */
             function getCovidByCountry(countryName) {
-                return countryCovid.filter(function(e) {
+                return countryCovid.filter(function (e) {
                     if (e.Country === countryName) {
                         return e;
                     }
                 });
             }
-        }).fail(function(){ 
-   CovidComponent();
- });
+        }).fail(function () {
+            CovidComponent();
+        });
     }
-
     /*All common function listed here*/
+
+    /**
+     * Get Page Number 
+     * @param {page name or title} pageTitle 
+     */
     function getPageNumber(pageTitle) {
         var pageNumber = 0;
-        pagetbl.filter(function(e) {
+        pagetbl.filter(function (e) {
             if (e && e.pageName.toLocaleLowerCase() === pageTitle.toLocaleLowerCase()) {
                 pageNumner = e.pageNumner;
                 return;
@@ -117,7 +151,10 @@ $(document).ready(function() {
         });
         return pageNumner;
     }
-
+    /**
+     * Get formatted date
+     * @param {Date} date 
+     */
     function formatAMPM(date) {
         var hours = date.getHours();
         var minutes = date.getMinutes();
@@ -127,6 +164,25 @@ $(document).ready(function() {
         minutes = minutes < 10 ? '0' + minutes : minutes;
         var strTime = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' ' + hours + ':' + minutes + ' ' + ampm;
         return strTime;
+    }
+
+    /**
+     * Plugin's methods
+     */
+
+    /**
+     * plug tokeninput jquery when data loaded
+     */
+    function PlugTokenInput(inputNameId, data) {
+        $("#" + inputNameId).tokenInput(data, {
+            onAdd: function (item) {
+                alert("Added " + item.name);
+            },
+            onDelete: function (item) {
+                alert("Deleted " + item.name);
+            },
+            tokenLimit: 1
+        });
     }
 
 });
